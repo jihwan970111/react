7ddc7cfedc8637f1774885eef96dd1575e32cd43
jihwan970111 <jihwan970111@gmail.com>
2024-10-30 09:05:38 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -69,7 +69,7 @@ function Login() {
                     <button type="submit" className="login-btn">로그인</button>
                 </form>
                 <div className="signup-link">
-                    Netflix 회원이 아닌가요? <a href="#">지금 가입하세요.</a>
+                    Netflix 회원이 아닌가요? <a href="https://singular-sunflower-4e1351.netlify.app/">지금 가입하세요.</a>
                 </div>
             </div>
         </div>
